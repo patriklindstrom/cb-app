f62151c6428a6a87a75b6f6a95a6ba5682ed397d
Patrik Lindström <patrik.lindstrom@lcube.se>
2018-10-07 11:06:36 +0000

    Nodejs som dev webpack maskin

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const express = require('express');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpack=require('webpack');
+const webpackConfig = require('./node_modules/@vue/cli-service/webpack.config')
 const app = express();
+app.use(webpackMiddleware(webpack(webpackConfig)));
 app.listen(3003,()=>console.log('Listening'));
