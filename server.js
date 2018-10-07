7986ab2b6551b664184905de005847266dd89028
Patrik Lindström <patrik.lindstrom@lcube.se>
2018-10-07 12:41:04 +0000

    Handle default prod default port and your devPort

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 
 const app = express();
+app.get('/hello'),(req,res)=> res.send({cm:'What superpower would you prefer, flight or invisiblity?'})
+app.get('/api'),(req,res)=> res.send({link:'./apidocumentation'})
+
 if(process.env.NODE_ENV !=='production'){
+    console.log('We are in development mode')
     const webpackMiddleware = require('webpack-dev-middleware');
     const webpack=require('webpack');
     const webpackConfig = require('./node_modules/@vue/cli-service/webpack.config')
     app.use(webpackMiddleware(webpack(webpackConfig)));
 }
 else {
+    console.log('We are in production mode')
     app.use(express.static('dist'));
     app.get('*',(req,res)=>{
         res.sendFile(path.join(__dirname,'dist/index.html'))
     })
 }
-app.listen(3003,()=>console.log('Listening'));
+app.listen(process.env.PORT || 3003,()=>console.log('Listening'));
