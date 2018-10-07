a39e367da0f7d2ce246fd6a632084d290694fe7f
Patrik Lindström <patrik.lindstrom@lcube.se>
2018-10-07 11:40:36 +0000

    Lagt till logik develop/prod nodejs --WIP

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack=require('webpack');
-const webpackConfig = require('./node_modules/@vue/cli-service/webpack.config')
+
 const app = express();
-app.use(webpackMiddleware(webpack(webpackConfig)));
+if(process.env.NODE_ENV !=='production'){
+    const webpackMiddleware = require('webpack-dev-middleware');
+    const webpack=require('webpack');
+    const webpackConfig = require('./node_modules/@vue/cli-service/webpack.config')
+    app.use(webpackMiddleware(webpack(webpackConfig)));
+}
+else {
+    app.use(express.static('dist'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'dist/index.html'))
+    })
+}
 app.listen(3003,()=>console.log('Listening'));
